f8b8d2e4c267d7cfcf8c9785e046a773d049ae3e
Alexander Herrera <alexcherrera@fullsail.edu>
2013-01-21 16:16:14 -0500

    Created new folder for my event planner app

---
+++ EventPlannerApp/EventPlannerApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EventPlannerApp
+//
+//  Created by Alexander Herrera on 1/21/13.
+//  Copyright (c) 2013 Alexander Herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
